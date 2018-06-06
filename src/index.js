7672fb2038e8c83b663267355cb556116c3f764c
nfort <laikisounds@gmail.com>
2018-06-06 13:14:38 +0300

    Rename js > ts

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import Map from './Map/Map';
-import Marker from './Marker/Marker';
-import Polygone from './Polygon/Polygon';
-
-export { Map, Marker, Polygone };
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import Map from "./Map/Map";
+import Marker from "./Marker/Marker";
+import Polygon from "./Polygon/Polygon";
+
+export { Map, Marker, Polygon };
