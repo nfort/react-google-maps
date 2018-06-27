9de61ef51bf65ee14db07511ec9497068e9c5c13
nfort <laikisounds@gmail.com>
2018-06-27 11:08:39 +0300

    Release 1.0.4

--- src/Polygon/Polygon.ts
+++ src/Polygon/Polygon.ts
@@ -6,16 +6,40 @@ interface Props {
   instance(polygon: google.maps.Polygon): void;
 }
 
-export default class Polygon extends React.Component<Props> {
+interface State {
+  polygon: null | google.maps.Polygon;
+}
+
+export default class Polygon extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      polygon: null
+    };
+  }
+
   setPolygon = () => {
     const { map, options, instance } = this.props;
     const polygon = new google.maps.Polygon(options);
     polygon.setMap(map!);
+    this.setState({ polygon });
     instance(polygon);
   };
 
   componentDidMount() {
     this.setPolygon();
+  }
+
+  shouldComponentUpdate(nextProps: Props) {
+    const { instance } = this.props;
+    const { polygon } = this.state;
+
+    if (polygon) {
+      polygon.setOptions(nextProps.options);
+      instance(polygon);
+    }
+
+    return true;
   }
 
   render() {
